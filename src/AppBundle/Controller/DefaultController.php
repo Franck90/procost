11700cd5032491888de8f9d08d6fcd9e7b4cbafd
Franck <franck.moniez7@etu.univ-lorraine.fr>
2018-04-19 10:33:44 +0200

    ez

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -13,42 +13,130 @@ class DefaultController extends Controller
      */
     public function dashboardAction(Request $request)
     {
-        $projects = $this->getDoctrine()->getRepository('AppBundle:Project')->findAll();
+        $projects = $this->getDoctrine()->getRepository('AppBundle:Project')->findBy(array(), array('date' => 'asc'));
         $employees = $this->getDoctrine()->getRepository('AppBundle:Employee')->findAll();
-        $details = $this->getDoctrine()->getRepository('AppBundle:Detail')->findAll();
+        $details = $this->getDoctrine()->getRepository('AppBundle:Detail')->findBy(array(), array('date' => 'asc'));
 
         $totalProjects = count($projects);
 
-        $projectsSend = array();
+        $totalSend = 0;
         $totalCapex = 0;
+
+
+        $projectsResumee = array();
+
         foreach ($projects as $project){
+
             if($project->getSend()){
-                array_push($projectsSend, $project);
+                $totalSend += 1;
             }
             if($project->getType() == "CAPEX"){
                 $totalCapex += 1;
             }
+
+            $costProject = 0;
+
+            foreach ($details as $detail)
+            {
+                if($detail->getProject()->getId() == $project->getId())
+                {
+                    $costProject += $detail->getDuration() * $detail->getEmployee()->getCost();
+                }
+            }
+
+            array_push($projectsResumee, array(
+                'id' => $project->getId(),
+                'name' => $project->getName(),
+                'type' => $project->getType(),
+                'date' => $project->getDate(),
+                'cost' => $costProject,
+                'send' => $project->getSend()
+            ));
         }
 
+        $lastProjects = array();
+
+        for( $i = 0 ; $i < 5 ; $i++)
+        {
+            $lastProject = array_pop($projectsResumee);
+
+            if($lastProject == null)
+            {
+                break;
+            }
+            array_push($lastProjects, $lastProject);
+        }
+
+        $percentSend = 0;
+        $percentCapex = 0;
+
+        if($totalProjects > 0)
+        {
+            $percentCapex = $totalCapex/$totalProjects * 100;
+            $percentSend = $totalSend/$totalProjects * 100;
+        }
+
+        $percentOpex = 100 - $percentCapex;
+        $percentNotSend = 100 - $percentSend;
+
+
         $duration = 0;
+
         foreach ($details as $detail)
         {
             $duration += $detail->getDuration();
         }
 
+        $lastDetails = array();
+        for( $i = 0 ; $i < 10 ; $i++)
+        {
+            $lastDetail = array_pop($details);
 
+            if($lastDetail == null)
+            {
+                break;
+            }
+            array_push($lastDetails, $lastDetail);
+        }
 
+        $topEmployee = 0;
+        $statEmployee = array();
 
+        foreach ($employees as $employee)
+        {
+            $costEmployee = 0;
 
-        // replace this example code with whatever you need
+            foreach ($details as $detail)
+            {
+                if( $employee->getId() == $detail->getEmployee()->getId())
+                {
+                    $costEmployee += $detail->getDuration() * $employee->getCost();
+                }
+            }
+            $statEmployee[$employee->getId()] = $costEmployee;
+        }
+
+        foreach ($employees as $employee)
+        {
+            if($employee->getId() == array_keys($statEmployee, max($statEmployee))[0] ){
+                $topEmployee = $employee;
+            }
+        }
+
         return $this->render('dashboard/dashboard.html.twig', array(
             'totalProjects' => $totalProjects,
-            'totalProjectsSend' => count($projectsSend),
+            'totalSend' => $totalSend,
             'totalEmployees' => count($employees),
             'duration' => $duration,
-            'totalCapex' => $totalCapex
+            'percentCapex' => round($percentCapex),
+            'percentOpex' => round($percentOpex),
+            'percentSend' => round($percentSend),
+            'percentNotSend' => round($percentNotSend),
+            'topEmployee' => $topEmployee,
+            'topCostEmployee' => max($statEmployee),
+            'lastProjects' => $lastProjects,
+            'lastDetails' => $lastDetails
         ));
     }
-
 
 }
